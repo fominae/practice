078c611eee990e5c3400bdb9b23f11215035dd9e
Your Name <your_email@whatever.com>
2024-04-11 17:36:47 +0700

    Реализация главной страницы

---
+++ views/site/home.php
@@ -0,0 +1,39 @@
+<div class="content">
+    <img class="image" src="/pop-it-mvc/public/media/marchcharact.jpg">
+    <h2>ГЛАВНАЯ ИНФОРМАЦИЯ</h2>
+    <div class="container">
+        <div class="article">
+            <h2>СОВЕТЫ ПО УПРАВЛЕНИЮ ПЕРСОНАЛОМ</h2>
+            <p>Успех компании зависит от её персонала, и от того — как он работает. Сотрудники организации хорошо
+                выполняют свои обязанности, если ими умело руководят...</p>
+        </div>
+        <div class="article">
+            <h2>ЦЕЛИ ОТДЕЛА КАДРОВ</h2>
+            <p>Цель отдела кадров - это способствование достижению целей предприятия (организации) путем обеспечения
+                предприятия необходимыми кадрами и эффективного использования потенциала работников...</p>
+        </div>
+        <div class="article">
+            <h2>ЗАДАЧИ ОТДЕЛА КАДРОВ</h2>
+            <p>Основная задача отдела кадров - правильно учитывать работу сотрудников, определять количество рабочих,
+                выходных и больничных дней для расчета зарплаты, отпусков и подачи сведений в бухгалтерию
+                организации...</p>
+        </div>
+    </div>
+</div>
+<footer class="footer">
+    <div class="footer_content">
+        <div class="footer_adress">
+            <p>Адрес: </br>Ивановская область, город Видное, проезд Будапештсткая, 89</p>
+        </div>
+        <div class="footer_information">
+        <div class="footer_info">
+            <p>pochta@mail.ru </p>
+            <img class="footer_image" src="/pop-it-mvc/public/media/free-icon-mail-icon-5757765.png">
+        </div>
+        <div class="footer_info">
+            <p>instagram.com</p>
+            <img class="footer_image" src="/pop-it-mvc/public/media/free-icon-photo-13266180.png">
+        </div>
+        </div>
+    </div>
+</footer>
