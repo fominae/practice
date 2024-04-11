4b4f7969c108ce41f87ac5f0927c366a535acb45
Your Name <your_email@whatever.com>
2024-04-11 21:54:40 +0700

    Добавление ссылок в навигацию

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,28 +1,47 @@
 <!DOCTYPE html>
-<html>
 <head>
     <title>Human Resources</title>
     <link rel="stylesheet" href="/pop-it-mvc/public/css/style.css">
 </head>
 <body>
 <div class="navbar">
-    <div class="title"><a class= "home_link" href="<?= app()->route->getUrl('/home') ?>"> Human Resources</a></div>
+    <div class="title"><a class="home_link" href="<?= app()->route->getUrl('/home') ?>"> Human Resources</a></div>
     <?php
     if (!app()->auth::check()):
         ?>
-        <button class="back"><a class= "login" href="<?= app()->route->getUrl('/login') ?>">Вход</a></button>
+
+        <button class="back"><a class="login" href="<?= app()->route->getUrl('/login') ?>">Вход</a></button>
     <?php
     else:
         ?>
-    <button class="back"><a class= "login" href="<?= app()->route->getUrl('/logout') ?>">Выход</a></button>
+    <div class="nav_link">
+               <?php
+        if(!app()->auth::checkRole()):
+            ?>
+            <a class="employer_link" href="<?= app()->route->getUrl('/employee') ?>">Сотрудники</a>
+        <?php
+        endif;
+               ?>
+        <?php
+        if(app()->auth::checkRole()):
+            ?>
+            <a class="employer_link" href="<?= app()->route->getUrl('/add_employee') ?>">Добавление нового сотрудника</a>
+        <?php
+        endif;
+        ?>
+
+        <button class="back"><a class="login" href="<?= app()->route->getUrl('/logout') ?>">Выход</a></button>
+</div>
     <?php
     endif;
     ?>
+
+
 
 </div>
 <main>
     <?= $content ?? '' ?>
 </main>
 </body>
-</html>
+
 
